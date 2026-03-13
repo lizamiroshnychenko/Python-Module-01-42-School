06ad250df0119869b702d635818421ed9bd91f14
Yelyzaveta Miroshnychenko <sansichek@gmail.com>
2026-03-13 21:44:22 +0100

    Update and rename ex00 to ft_garden_intro.py

---
+++ ft_garden_intro.py
@@ -0,0 +1,16 @@
+def ft_garden_intro() -> None:
+    """Displays basic information from a plant"""
+    plant = "Rose"
+    height = 25
+    age = 30
+    print(
+        "=== Welcome to My Garden ===\n"
+        f"Plant: {plant}\n"
+        f"Height: {height}cm\n"
+        f"Age: {age} days\n\n"
+        "=== End of Program ==="
+    )
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
